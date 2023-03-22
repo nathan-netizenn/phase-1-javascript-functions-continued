f749850147968b56dfb6cf873ff3c441ea81e8ed
nathan <nathanmageka6@gmail.com>
2023-03-22 12:12:08 +0300

    nathan

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun() {
+    console.log("This Saturday, I want to have Fun!");
+}
+saturdayFun();
+//=> "This Saturday, I want to have Fun!"
+function saturdayFun(opt="roller-skate"){
+    return `This Saturday, I want to ${opt}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(ala="go to the office"){
+    return `This Monday, I will ${ala}.`;}
+
+    function wrapAdjective(adjective="*"){
+        const innerF= function(para="a hardworker"){
+            return `You are ${adjective}${para}${adjective}!`;
+        }
+        return innerF;
+    }
